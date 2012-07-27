2b2fe50803a2d7426d83b8fe7d5d826ab03e79ae
Devaraj Rangasamy <dev@ti.com>
2012-07-26 19:45:27 -0500

    merge w/ omapzoom
    Change-Id: Ic96222a6b417ed310de491fc7e286ff9484dab74
    
    Conflicts:
    
    	media/libstagefright/colorconversion/Android.mk

--- media/libstagefright/colorconversion/Android.mk
+++ media/libstagefright/colorconversion/Android.mk
@@ -11,10 +11,12 @@ LOCAL_C_INCLUDES := \
 
 LOCAL_MODULE:= libstagefright_color_conversion
 
-ifdef ENHANCED_DOMX
-LOCAL_C_INCLUDES += $(DOMX_PATH)/omx_core/inc
+ifeq ($(OMAP_ENHANCEMENT), true)
+ifeq ($(ENHANCED_DOMX), true)
+LOCAL_C_INCLUDES += $(TOP)/hardware/ti/domx/omx_core/inc
 else
 LOCAL_C_INCLUDES += $(TOP)/hardware/ti/omap4xxx/domx/omx_core/inc
+endif
 endif
 
 include $(BUILD_STATIC_LIBRARY)
